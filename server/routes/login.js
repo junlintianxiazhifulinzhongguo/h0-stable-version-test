f472bf8966516fe49ee2073ae76da31a8c84fc0c
root <root@localhost.localdomain>
2019-01-13 07:03:10 -0500

    33dgfsdddddd

--- server/routes/login.js
+++ server/routes/login.js
@@ -39,13 +39,6 @@ export class loginController
         // www.junlintianxiazhifulinzhongguo.top/api/v0/login/authRedirect?app_id=2018123062714467&source=alipay_wallet&scope=auth_user&auth_code=05cfa47b90104c9eae6d760c983dOX57
         const { app_id,source,scope,auth_code } = ctx.query
         console.log(auth_code)
-        // "accessToken": "authusrBa5492ef2af1f4e22ac26bcd3d7f9eX57",
-        // "alipayUserId": "20880002728689039857121792515757",
-        // "expiresIn": 1296000,
-        // "reExpiresIn": 2592000,
-        // "refreshToken": "authusrB88a548e0513c4ffd8de8ed74c9e1aX57",
-        // "userId": "2088902939066578"
-        // const auth_code = '5268dd2fb0214e51824377435dd6QX57'
         // const { access_token,alipay_user_id,expires_in,re_expires_in,refresh_token,user_id } = await access_token(auth_code)
         // const result = await user_info(access_token)
         // const [ accessToken,alipayUserId,expiresIn,reExpiresIn,refreshToken,userId ] = await access_token(auth_code)
@@ -55,7 +48,7 @@ export class loginController
         //     result
         // }
         ctx.body = {
-            accessToken,alipayUserId,expiresIn,reExpiresIn,refreshToken,userId
+            result
         }
     }   
 }
