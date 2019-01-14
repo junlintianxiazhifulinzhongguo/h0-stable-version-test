5050ce6ccf0c2bea84c3297110f254d6143f92fa
root <root@localhost.localdomain>
2019-01-13 20:18:06 -0500

    dddda

--- server/routes/login.js
+++ server/routes/login.js
@@ -52,5 +52,7 @@ export class loginController
         // ctx.body = {
         //     result
         // }
+
+        // {roles: Array(1), token: "admin", introduction: "我是超级管理员", avatar: "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif", name: "Super Admin"}
     }   
 }
