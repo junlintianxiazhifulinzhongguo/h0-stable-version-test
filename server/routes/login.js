ff2c29644d99d9d5501d2c66299604e2ebf92d8c
root <root@localhost.localdomain>
2019-01-16 08:00:08 -0500

    ddaddddddaaasa

--- server/routes/login.js
+++ server/routes/login.js
@@ -47,15 +47,16 @@ export class loginController
         const result_Info = await getUserInfo(result_token.accessToken)
        // console.log(JSON.stringify(result_Info))
         const result =JSON.stringify(result_Info)
-        //ctx.redirect('http://www.junlintianxiazhifulinzhongguo.top/#/auth-redirect'+JSON.stringify(result_Info));
+        const auth_redirect = 'http://www.junlintianxiazhifulinzhongguo.top/#/auth-redirect' + result
+        ctx.redirect(auth_redirect);
         //console.log(5555)  
        // console.log(result)
         // console.log(result_Info)    
         
         
         // const result = {result_Info,roles:  ["admin"], token: "admin", introduction: "我是超级管理员", avatar: "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif", name: "Super Admin"}
-        ctx.body = {
-            result
-        }
+        // ctx.body = {
+        //     result
+        // }
     }   
 }
