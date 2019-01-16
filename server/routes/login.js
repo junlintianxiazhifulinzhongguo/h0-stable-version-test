6790d91aed03ede239c1d932710192c124e288f1
root <root@localhost.localdomain>
2019-01-16 05:37:39 -0500

    ddadddddd

--- server/routes/login.js
+++ server/routes/login.js
@@ -38,20 +38,20 @@ export class loginController
     async getAlipayAuthUrlById(ctx,next)
     {
         // www.junlintianxiazhifulinzhongguo.top/api/v0/login/authRedirect?app_id=2018123062714467&source=alipay_wallet&scope=auth_user&auth_code=05cfa47b90104c9eae6d760c983dOX57
-        // const { app_id,source,scope,auth_code } = ctx.query
+        const { app_id,source,scope,auth_code } = ctx.query
         // console.log(auth_code)
         // const { access_token,alipay_user_id,expires_in,re_expires_in,refresh_token,user_id } = await access_token(auth_code)
          
         // const [ accessToken,alipayUserId,expiresIn,reExpiresIn,refreshToken,userId ] = await access_token(auth_code)
-        // const result_token = await getAccessToken(auth_code)
-        // const result_Info = await getUserInfo(result_token.accessToken)
+        const result_token = await getAccessToken(auth_code)
+        const result_Info = await getUserInfo(result_token.accessToken)
         // ctx.redirect('http://www.junlintianxiazhifulinzhongguo.top/login/success',result);
         //console.log(5555)  
        // console.log(result)
         // console.log(result_Info)    
         
         
-        const result = {roles:  ["admin"], token: "admin", introduction: "我是超级管理员", avatar: "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif", name: "Super Admin"}
+        const result = {result_Info,roles:  ["admin"], token: "admin", introduction: "我是超级管理员", avatar: "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif", name: "Super Admin"}
         ctx.body = {
             result
         }
