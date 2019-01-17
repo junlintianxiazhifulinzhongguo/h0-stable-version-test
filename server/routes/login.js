e71dc7f2a0aa8547d230957738ccec64c2860659
root <root@localhost.localdomain>
2019-01-17 08:22:39 -0500

    aaf

--- server/routes/login.js
+++ server/routes/login.js
@@ -33,7 +33,17 @@ export class loginController
             auth_url
         }
     }
-
+    @get("/getToken")
+    getToken(ctx,next)
+    {
+        ctx.body = {
+            roles:  ["admin"],
+            token: "admin", 
+            introduction: "我是超级管理员", 
+            avatar: "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif", 
+            name: "Super Admin"
+        }
+    }
     @get("/authRedirect")
     async getAlipayAuthUrlById(ctx,next)
     {
