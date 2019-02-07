4da7f3cbfe983c9033547acf943ced7d6f843809
root <root@localhost.localdomain>
2019-02-07 11:27:59 -0500

    1121345212

--- server/service/common.js
+++ server/service/common.js
@@ -3,19 +3,7 @@ const Administrators = mongoose.model("Administrators");
 const checkIsRegisterBythirdpart = async (thirdpart,id) => {
    console.log(thirdpart)
    console.log(id)
-   
-   switch(thirdpart)
-   {
-        case 'alipayUserId':
-            const result = await Administrators.find({ 'alipayUserId' : id})
-            return result;
-        case 'qqUserId':
-            result = await Administrators.find({ 'qqUserId' : id})
-            return result;
-        default:
-            result = await Administrators.find({ 'wechatUserId' : id})
-            return result;    
-   }
+   const result = await Administrators.find({ 'alipayUserId' : id})
    console.log(result)
    if (result)
    {
