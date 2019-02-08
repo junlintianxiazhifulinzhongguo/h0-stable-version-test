86bd05d063393eb42b8f1565d0835a499e01dd7b
root <root@localhost.localdomain>
2019-02-08 03:10:23 -0500

    w12122131

--- server/routes/login.js
+++ server/routes/login.js
@@ -80,9 +80,9 @@ export class loginController
     async getToken(ctx,next)
     {
         console.log(ctx.request.body)
-        // const { type,value } = ctx.params
-        // console.log(ctx.params)
-        // console.log(type,value)
+        const { type,value } = ctx.request.body
+        console.log(type)
+        console.log(value)
         // const token = await getToken(type,value)
         ctx.body = {
             "name":"lijun1"
