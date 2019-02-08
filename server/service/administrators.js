450c065e5bede97098f7b8fd39e2dd0488d95906
root <root@localhost.localdomain>
2019-02-07 23:51:42 -0500

    3452121s2w1212213

--- server/service/administrators.js
+++ server/service/administrators.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose'
-import {  
-    checkIsRegisterBythirdpart
-} from './common'
 const Administrators = mongoose.model("Administrators");
+
 const getAll = async () =>{
     let query = {}
     const result =await Administrators.find(query)
     return result
 }
+
 const  getByUsername = async (username) =>{
     let query = {}
     if(username)
@@ -17,10 +16,11 @@ const  getByUsername = async (username) =>{
     const result =await Administrators.find(query)
     return result
 }
+
 const  addUserByAlipay = async (resultInfo) =>{
-    const is_true = await checkIsRegisterBythirdpart('alipayUserId',resultInfo.userId)
-    console.log(is_true,222)
-    if(!is_true)
+    const isExistence = await Administrators.find({ 'alipayUserId':resultInfo.userId })
+    console.log(isExistence)
+    if (!isExistence.length)
     {
         const administrators = new Administrators({  
             name: resultInfo.nickName,
@@ -29,10 +29,11 @@ const  addUserByAlipay = async (resultInfo) =>{
             alipayUserId: resultInfo.userId
         });
         await administrators.save();
-    }   
-    const result =await Administrators.findOne({'alipayUserId':resultInfo.userId})
+    }  
+    const result = await Administrators.findOne({'alipayUserId':resultInfo.userId})
     return result
 }
+
 export {
     getAll,
     getByUsername,
